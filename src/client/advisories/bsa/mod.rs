96e59993540b688e5005b07e87d60be877e12133
Dylan Frankland <dfrankland@users.noreply.github.com>
2019-11-03 00:04:39 -0700

    Add advisories BSA

---
+++ src/client/advisories/bsa/mod.rs
@@ -0,0 +1,61 @@
+pub mod r#type;
+
+use crate::client::constants::PUBLIC_KEY;
+use serde::{Serialize, Deserialize, Deserializer};
+use reqwest;
+use anyhow::Result;
+use self::r#type::BsaType;
+
+fn extract_cdata_section<'de, D>(deserializer: D) -> std::result::Result<String, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    #[derive(Deserialize)]
+    struct CDATASection {
+        #[serde(rename = "#cdata-section")]
+        inner: String,
+    }
+    CDATASection::deserialize(deserializer).map(|cdata_section| cdata_section.inner)
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Bsa {
+    #[serde(rename = "@id")]
+    pub id: Option<String>,
+    pub station: String,
+    pub r#type: Option<BsaType>,
+    #[serde(deserialize_with = "extract_cdata_section")]
+    pub description: String,
+    #[serde(deserialize_with = "extract_cdata_section")]
+    pub sms_text: String,
+    pub posted: Option<String>, // Should be a chrono DateTime
+    pub expires: Option<String>, // Should be a chrono DateTime
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct BsaResponse {
+    pub date: String, // Should be a chrono NaiveDate
+    pub time: String, // Should be a chrono NaiveTime
+    pub bsa: Vec<Bsa>,
+    pub message: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+struct Root {
+    pub root: BsaResponse,
+}
+
+pub fn url<T: AsRef<str>>(key: Option<T>) -> String {
+    format!("https://api.bart.gov/api/bsa.aspx?cmd=bsa&key={}&json=y", key.map(|k| String::from(k.as_ref())).unwrap_or_else(|| String::from(PUBLIC_KEY)))
+}
+
+pub async fn call<T: AsRef<str>>(key: Option<T>) -> Result<BsaResponse> {
+    let root = reqwest::get(&url(key)).await?.json::<Root>().await?;
+    Ok(root.root)
+}
+
+#[tokio::test]
+async fn version() {
+    let reponse = call::<&str>(None).await.unwrap();
+    assert_eq!(reponse.time.is_empty(), false);
+}
---
+++ src/client/advisories/bsa/type.rs
@@ -0,0 +1,54 @@
+use serde::{Serialize, Serializer, Deserialize, Deserializer};
+use std::convert::TryFrom;
+use anyhow::{Result, anyhow};
+
+pub const BSA_TYPE_DELAY: &str = "DELAY";
+pub const BSA_TYPE_EMERGENCY: &str = "EMERGENCY";
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum BsaType {
+    Delay,
+    Emergency
+}
+
+impl BsaType {
+    pub fn from_code<T: AsRef<str>>(code: T) -> Result<BsaType> {
+        match code.as_ref() {
+            BSA_TYPE_DELAY => Ok(BsaType::Delay),
+            BSA_TYPE_EMERGENCY => Ok(BsaType::Emergency),
+            _ => Err(anyhow!("Does not match any BSA type")),
+        }
+    }
+
+    pub fn to_code(&self) -> &'static str {
+        match self {
+            BsaType::Delay => BSA_TYPE_DELAY,
+            BsaType::Emergency => BSA_TYPE_EMERGENCY,
+        }
+    }
+}
+
+impl TryFrom<String> for BsaType {
+    type Error = anyhow::Error;
+
+    fn try_from(bsa_type_string: String) -> std::result::Result<Self, Self::Error> {
+        BsaType::from_code(&bsa_type_string)
+    }
+}
+
+impl Serialize for BsaType {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer
+    {
+        serializer.serialize_str(self.to_code())
+    }
+}
+
+impl<'de> Deserialize<'de> for BsaType {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where D: Deserializer<'de>
+    {
+        let s = String::deserialize(deserializer)?;
+        BsaType::try_from(s).map_err(serde::de::Error::custom)
+    }
+}
